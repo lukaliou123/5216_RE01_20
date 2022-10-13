dc3619d6f72b0fed17d7488da2b05a4c9d4b0338
chenjiaxu <lukaliou321@gmail.com>
2022-10-13 16:33:08 +0800

    update service json

--- app/src/main/java/comp5216/sydney/edu/au/afinal/MapsActivity.java
+++ app/src/main/java/comp5216/sydney/edu/au/afinal/MapsActivity.java
@@ -3,13 +3,17 @@ package comp5216.sydney.edu.au.afinal;
 import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
 
 import comp5216.sydney.edu.au.afinal.databinding.ActivityMapsBinding;
 
@@ -17,6 +21,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
+
+    // below are the latitude and longitude
+    // of 4 different locations.
+    LatLng sydney = new LatLng(-34, 151);
+    LatLng TamWorth = new LatLng(-31.083332, 150.916672);
+    LatLng NewCastle = new LatLng(-32.916668, 151.750000);
+    LatLng Brisbane = new LatLng(-27.470125, 153.021072);
+
+    // two array list for our lat long and location Name;
+    private ArrayList<LatLng> latLngArrayList;
+    private ArrayList<String> locationNameArraylist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +44,21 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        // initializing our array lists.
+        latLngArrayList = new ArrayList<>();
+        locationNameArraylist = new ArrayList<>();
+
+        // on below line we are adding
+        // data to our array list.
+        latLngArrayList.add(sydney);
+        locationNameArraylist.add("Sydney");
+        latLngArrayList.add(TamWorth);
+        locationNameArraylist.add("TamWorth");
+        latLngArrayList.add(NewCastle);
+        locationNameArraylist.add("New Castle");
+        latLngArrayList.add(Brisbane);
+        locationNameArraylist.add("Brisbase");
     }
 
     /**
@@ -45,8 +75,29 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//        LatLng sydney = new LatLng(-34, 151);
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // below line is to add marker to google maps
+        for (int i = 0; i < latLngArrayList.size(); i++) {
+
+            // adding marker to each location on google maps
+            mMap.addMarker(new MarkerOptions().position(latLngArrayList.get(i)).title("Marker in " + locationNameArraylist.get(i)));
+
+            // below line is use to move camera.
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        }
+
+        // adding on click listener to marker of google maps.
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                // on marker click we are getting the title of our marker
+                // which is clicked and displaying it in a toast message.
+                String markerName = marker.getTitle();
+                Toast.makeText(MapsActivity.this, "Clicked location is " + markerName, Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
     }
 }
