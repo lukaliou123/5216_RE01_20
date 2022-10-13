7d939015fbd811b6bc95b716d5a2e051fc01b42b
jiahui Li <jili8323@uni.sydney.edu.au>
2022-10-14 04:30:06 +0800

    adding navigator ui

---
+++ app/src/main/java/comp5216/sydney/edu/au/afinal/ui/search/SearchViewModel.java
@@ -0,0 +1,19 @@
+package comp5216.sydney.edu.au.afinal.ui.search;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SearchViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public SearchViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is search fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
