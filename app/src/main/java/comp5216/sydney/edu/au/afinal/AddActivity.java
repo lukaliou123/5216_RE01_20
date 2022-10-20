fb085d8989a612f6a566410abc5d18cb59e5ed92
jiahui Li <jili8323@uni.sydney.edu.au>
2022-10-20 22:56:26 +0800

    fixed bug in add

--- app/src/main/java/comp5216/sydney/edu/au/afinal/AddActivity.java
+++ app/src/main/java/comp5216/sydney/edu/au/afinal/AddActivity.java
@@ -25,6 +25,7 @@ import android.provider.MediaStore;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -36,6 +37,7 @@ import com.google.firebase.Timestamp;
 import com.google.firebase.firestore.GeoPoint;
 import com.makeramen.roundedimageview.RoundedImageView;
 import com.youth.banner.Banner;
+import com.youth.banner.indicator.CircleIndicator;
 import com.youth.banner.listener.OnBannerListener;
 
 import java.io.File;
@@ -147,8 +149,6 @@ public class AddActivity extends AppCompatActivity {
                    if(task.isSuccessful()){
                        urls.add(task.getResult().toString());
                        if(finalI == photoPath.size() - 1){
-//                           EventEntity event = new EventEntity("test","??",description,new Timestamp(new Date()),geo,
-//                                   urls,0,address,titleVal);
                            EventEntity event = new EventEntity(curUser.getUsername(),curUser.getAccountID(),description,
                                    new Timestamp(new Date()),geo, urls,0,address,titleVal);
                            NetUtil.uploadEvent(event, AddActivity.this);
@@ -167,6 +167,7 @@ public class AddActivity extends AppCompatActivity {
         startActivityForResult(intent, 1);
     }
 
+    @SuppressLint("NotifyDataSetChanged")
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -188,8 +189,7 @@ public class AddActivity extends AppCompatActivity {
                 image = BitmapFactory.decodeFile(picturePath);
                 photoPath.add(picturePath);
                 imageList.add(image);
-
-            }
+            }bannerAdapter.notifyDataSetChanged();
 
         }
 
